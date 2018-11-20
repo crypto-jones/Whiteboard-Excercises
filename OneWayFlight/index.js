931f5f008d133534bd000d09db5ba60ac5e0ca3d
crypto-jones <integral.being@gmail.com>
2018-11-19 21:59:49 -0800

    Add alternate solution for OneWayFlight

--- OneWayFlight/index.js
+++ OneWayFlight/index.js
@@ -1,20 +1,58 @@
-function reconstructTrip(tix) {
-  const hashMap = {};
-  const path = [];
+// // O(n) Solution
 
-  for (let i in tix) {
-    if (tix[i][0] === null) {
-      path[0] = tix[i][1];
+// function reconstructTrip(tix) {
+//   const hashMap = {};
+//   const path = [];
+
+//   for (let i in tix) {
+//     if (tix[i][0] === null) {
+//       path[0] = tix[i][1];
+//     }
+
+//     hashMap[tix[i][0]] = tix[i][1];
+//   }
+
+//   for (let i = 1; i < tix.length - 1; i++) {
+//     path[i] = hashMap[path[i - 1]];
+//   }
+
+//   return path;
+// }
+
+// O(n^2) Solution
+
+function reconstructTrip(tickets) {
+  // initialize empty arr
+  let route = [];
+
+  for (let i in tickets) {
+    // find origin and push it onto route arr
+    if (tickets[i][0] === null) {
+      route.push(tickets[i][1]);
+      tickets.splice(i, 1);
     }
 
-    hashMap[tix[i][0]] = tix[i][1];
+    // find destination and splice it from tickets arr
+    if (tickets[i][1] === null) {
+      tickets.splice(i, 1);
+    }
   }
 
-  for (let i = 1; i < tix.length - 1; i++) {
-    path[i] = hashMap[path[i - 1]];
-  }
+  console.log(route);
 
-  return path;
+  // loop through tickets arr and compare last element
+  // of route arr with each element of tickets arr
+  // if match push the element onto the route arr
+  while (tickets.length > 0) {
+    for (let i in tickets) {
+      if (tickets[i][0] === route[route.length - 1]) {
+        route.push(tickets[i][1]);
+        tickets.splice(i, 1);
+      }
+    }
+  }
+  console.log(route);
+  return route;
 }
 
 const shorterSet = [[null, 'PDX'], ['PDX', 'DCA'], ['DCA', null]];
